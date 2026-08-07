07cc7685daf1088e35b728d6f00c81487e957e09
Anirudh <anirudh@gmail.com>
2026-08-07 15:21:12 +0530

    Add Student model

---
+++ lib/models/student.dart
@@ -0,0 +1,15 @@
+class Student {
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String course;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.course,
+  });
+}
